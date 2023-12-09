bbd6b27318b2c0bf3e1b96eab97d8d559cf2617c
Marcus  Roza <mavirolero@gmail.com>
2023-12-09 15:38:19 -0300

    atualizando dialog

--- src/components/DialogMenu.tsx
+++ src/components/DialogMenu.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "../context/theme";
 import Nav from "./Nav";
 
 interface PropsDialog {
@@ -9,13 +10,14 @@ export default function RenderDialog({
   toggleMobileMenu,
   renderNavLinks,
 }: PropsDialog) {
+  const {isDarkMode} = useTheme();
   return (
-    <dialog className="top-0 left-0 min-w-screen min-h-screen lg:hidden bg-dark bg-opacity-0 z-20 relative overflow-hidden">
-      <div className="w-screen min-h-screen flex justify-center items-center bg-dark bg-opacity-0 ">
+    <dialog className={`top-0 left-0 min-w-screen min-h-screen lg:hidden ${!isDarkMode ? 'bg-dark'  : 'bg-light'} bg-opacity-0 z-20 relative overflow-hidden`}>
+      <div className={`w-screen min-h-screen flex justify-center items-center ${!isDarkMode ? 'bg-dark'  : 'bg-light'} bg-opacity-0 `}>
         <div className="bg-white w-[18.75rem] h-[31.25rem] flex flex-col justify-center items-center rounded-xl relative ">
           <strong
             onClick={toggleMobileMenu}
-            className="text-dark font-extrabold font-[main] absolute left-5 top-5 text-[1.5rem] cursor-pointer"
+            className={`${!isDarkMode ? 'text-dark' : 'text-light'} font-extrabold font-[main] absolute left-5 top-5 text-[1.5rem] cursor-pointer`}
           >
             {"X"}
           </strong>
